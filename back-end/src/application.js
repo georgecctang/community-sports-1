65c34a3292a300f30aefcc59859f0d64fb8547d3
Bill Watters <billywatters13@gmail.com>
2020-11-20 20:03:09 -0500

    Setup Backend

--- back-end/src/application.js
+++ back-end/src/application.js
@@ -11,23 +11,21 @@ const app = express();
 const db = require("./db");
 
 const days = require("./routes/days");
-const appointments = require("./routes/appointments");
-const interviewers = require("./routes/interviewers");
 
-function read(file) {
-  return new Promise((resolve, reject) => {
-    fs.readFile(
-      file,
-      {
-        encoding: "utf-8"
-      },
-      (error, data) => {
-        if (error) return reject(error);
-        resolve(data);
-      }
-    );
-  });
-}
+// function read(file) {
+//   return new Promise((resolve, reject) => {
+//     fs.readFile(
+//       file,
+//       {
+//         encoding: "utf-8"
+//       },
+//       (error, data) => {
+//         if (error) return reject(error);
+//         resolve(data);
+//       }
+//     );
+//   });
+// }
 
 module.exports = function application(
   ENV,
@@ -38,28 +36,26 @@ module.exports = function application(
   app.use(bodyparser.json());
 
   app.use("/api", days(db));
-  app.use("/api", appointments(db, actions.updateAppointment));
-  app.use("/api", interviewers(db));
 
-  if (ENV === "development" || ENV === "test") {
-    Promise.all([
-      read(path.resolve(__dirname, `db/schema/create.sql`)),
-      read(path.resolve(__dirname, `db/schema/${ENV}.sql`))
-    ])
-      .then(([create, seed]) => {
-        app.get("/api/debug/reset", (request, response) => {
-          db.query(create)
-            .then(() => db.query(seed))
-            .then(() => {
-              console.log("Database Reset");
-              response.status(200).send("Database Reset");
-            });
-        });
-      })
-      .catch(error => {
-        console.log(`Error setting up the reset route: ${error}`);
-      });
-  }
+  // if (ENV === "development" || ENV === "test") {
+  //   Promise.all([
+  //     read(path.resolve(__dirname, `db/schema/create.sql`)),
+  //     read(path.resolve(__dirname, `db/schema/${ENV}.sql`))
+  //   ])
+  //     .then(([create, seed]) => {
+  //       app.get("/api/debug/reset", (request, response) => {
+  //         db.query(create)
+  //           .then(() => db.query(seed))
+  //           .then(() => {
+  //             console.log("Database Reset");
+  //             response.status(200).send("Database Reset");
+  //           });
+  //       });
+  //     })
+  //     .catch(error => {
+  //       console.log(`Error setting up the reset route: ${error}`);
+  //     });
+  // }
 
   app.close = function() {
     return db.end();
