f7f00cf81a9d1befa9940f8adba9f5f8466697b9
Bill Watters <billywatters13@gmail.com>
2020-11-21 18:39:39 -0500

    register now will not create a new user if email in use

--- back-end/src/routes/register.js
+++ back-end/src/routes/register.js
@@ -4,25 +4,33 @@ const router = require("express").Router()
 module.exports = db => {
   router.post("/register", (req, res) => {
     const {first_name, last_name, email, password, phone, age, gender} = req.body
-    console.log(req.body)
-    console.log(first_name, last_name, email, password, phone, age, gender)
-    //The on conflict is not working right now
-    // `SELECT * FROM users WHERE email=$1`
-    db.query(`
-      INSERT INTO users (first_name, last_name, email, password, phone, age, gender) 
-      VALUES ($1::text, $2::text, $3::text, $4::text, $5::text, $6::integer, $7::text)
-      RETURNING id;
-      `
-      , [first_name, last_name, email, password, phone, age, gender ]
-    ) .then((data) => { 
-        const id = data.rows[0].id
-        req.session.user_id = id //saves user as as cookie
-        res.json('Updated') 
-        return data
-      }) 
-      .catch((err) => {
-        console.log(err)
-      })
+    db.query(`SELECT * FROM users WHERE email=$1`, 
+    [email]) 
+    .then((user) => {
+      console.log(user.rows[0])
+      //If user exists stop and send email already exists
+      if (user.rows[0]) {
+        return 'Email already in use'
+        //if user email does not exist add to DB
+      } else {
+        db.query(`
+          INSERT INTO users (first_name, last_name, email, password, phone, age, gender) 
+          VALUES ($1::text, $2::text, $3::text, $4::text, $5::text, $6::integer, $7::text)
+          RETURNING id;
+          `
+          , [first_name, last_name, email, password, phone, age, gender ]
+        ) .then((data) => { 
+            const id = data.rows[0].id
+            req.session.user_id = id //saves user as as cookie
+            res.json('Updated') 
+            //returning the whole user object 
+            return data
+         }) 
+          .catch((err) => {
+            console.log(err)
+          })
+        }
+      })  
     })
   return router;
 }
