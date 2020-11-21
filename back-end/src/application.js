47da27b5859224a3027d06795f289d9fb97679bc
George Tang <georgecctang@gmail.com>
2020-11-20 20:11:55 -0500

    change made

--- back-end/src/application.js
+++ back-end/src/application.js
@@ -41,25 +41,25 @@ module.exports = function application(
   app.use("/api", appointments(db, actions.updateAppointment));
   app.use("/api", interviewers(db));
 
-  if (ENV === "development" || ENV === "test") {
-    Promise.all([
-      read(path.resolve(__dirname, `db/schema/create.sql`)),
-      read(path.resolve(__dirname, `db/schema/${ENV}.sql`))
-    ])
-      .then(([create, seed]) => {
-        app.get("/api/debug/reset", (request, response) => {
-          db.query(create)
-            .then(() => db.query(seed))
-            .then(() => {
-              console.log("Database Reset");
-              response.status(200).send("Database Reset");
-            });
-        });
-      })
-      .catch(error => {
-        console.log(`Error setting up the reset route: ${error}`);
-      });
-  }
+  // if (ENV === "development" || ENV === "test") {
+  //   Promise.all([
+  //     read(path.resolve(__dirname, `db/schema/create.sql`)),
+  //     read(path.resolve(__dirname, `db/schema/${ENV}.sql`))
+  //   ])
+  //     .then(([create, seed]) => {
+  //       app.get("/api/debug/reset", (request, response) => {
+  //         db.query(create)
+  //           .then(() => db.query(seed))
+  //           .then(() => {
+  //             console.log("Database Reset");
+  //             response.status(200).send("Database Reset");
+  //           });
+  //       });
+  //     })
+  //     .catch(error => {
+  //       console.log(`Error setting up the reset route: ${error}`);
+  //     });
+  // }
 
   app.close = function() {
     return db.end();
