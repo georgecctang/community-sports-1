1b9ec39221118c67e044b2447fb54b3ecdf0e266
Bill Watters <billywatters13@gmail.com>
2020-11-22 18:58:46 -0500

    Gets long lat from google api and stores it in event table

--- back-end/src/routes/owners.js
+++ back-end/src/routes/owners.js
@@ -1,32 +1,47 @@
-
 const router = require("express").Router(); 
+const axios = require("axios")
 
 module.exports = db => {  
   router.post("/owners/events/new", (req, res) => {
     const {owner_id , date, start_time, end_time, title,
       address, city, province, current_participants, max_participants, skill_level, 
-      gender_restriction, referee, additional_info} = req.body 
-      
-      db.query(` 
-      INSERT INTO events (owner_id , date, start_time, end_time, title,
-        address, city, province, current_participants, max_participants, skill_level, 
-        gender_restriction, referee, additional_info) 
-        VALUES ($1::integer, $2::date, $3::time, $4::time, $5::text, $6::text, 
-        $7::text, $8::text, $9::integer, $10::integer, $11::text, $12::text, $13::boolean, $14::text)
-        RETURNING owner_id , date, start_time, end_time, title,
-        address, city, province, current_participants, max_participants, skill_level, 
-        gender_restriction, referee, additional_info;`
-        , [owner_id, date, start_time, end_time, title, address, city, province, current_participants, 
-        max_participants, skill_level, gender_restriction, referee, additional_info]
-        ) .then(() => {
-          //When it saved
-          res.send('Successfully Created')
-        }) 
-        //When it fails
-        .catch((err) => {
-          console.log(err)
-          res.send('Error')
-        })
+      gender_restriction, referee, additional_info} = req.body  
+
+      let location 
+       
+      const geocodeAddress = address.replace(/\s/g, '+') 
+      const geocodeCity = city.replace(/\s/g, '+') 
+      //Retriving lat and long from google api
+      axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${geocodeAddress},+${geocodeCity},+CA&key=AIzaSyCiH--m_9xGKiwCJxv9nIgg1PYanFgTNL8`)
+      .then((res) => {
+        const lat = res.data.results[0].geometry.location.lat
+        const long = res.data.results[0].geometry.location.lng
+        location = `(${lat}, ${long})` 
+      })
+      .then(() => console.log(location)) 
+      .then(() => {
+        db.query(` 
+        INSERT INTO events (owner_id , date, start_time, end_time, title,
+          address, city, province, current_participants, max_participants, skill_level, 
+          gender_restriction, referee, additional_info, location) 
+          VALUES ($1::integer, $2::date, $3::time, $4::time, $5::text, $6::text, 
+          $7::text, $8::text, $9::integer, $10::integer, $11::text, $12::text, $13::boolean, $14::text, $15::point)
+          RETURNING owner_id , date, start_time, end_time, title,
+          address, city, province, current_participants, max_participants, skill_level, 
+          gender_restriction, referee, additional_info;`
+          , [owner_id, date, start_time, end_time, title, address, city, province, current_participants, 
+          max_participants, skill_level, gender_restriction, referee, additional_info, location]
+        ) 
+      })
+      .then(() => {
+        //When it saved
+        res.send('Successfully Created')
+      })
+      //When it fails
+      .catch((err) => {
+        console.log(err)
+        res.send('Error')
+      })
   }) 
 
   router.delete("/owners/events/:id/delete", (req, res) => {
