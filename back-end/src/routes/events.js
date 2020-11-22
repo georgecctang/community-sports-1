d12eb5e49491b87602b888de51b5f9cdc10b5ce1
Bill Watters <billywatters13@gmail.com>
2020-11-21 20:59:22 -0500

    Change date to type date

--- back-end/src/routes/events.js
+++ back-end/src/routes/events.js
@@ -10,7 +10,7 @@ module.exports = db => {
       INSERT INTO events (owner_id , date, start_time, end_time, title,
         address, city, province, current_participants, max_participants, skill_level, 
         gender_restriction, referee, additional_info) 
-        VALUES ($1::integer, $2::text, $3::time, $4::time, $5::text, $6::text, 
+        VALUES ($1::integer, $2::date, $3::time, $4::time, $5::text, $6::text, 
         $7::text, $8::text, $9::integer, $10::integer, $11::text, $12::text, $13::boolean, $14::text)
         RETURNING owner_id , date, start_time, end_time, title,
         address, city, province, current_participants, max_participants, skill_level, 
