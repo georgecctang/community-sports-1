3b0c1c36dd9ce8265f58491663ed7c8219748167
George Tang <georgecctang@gmail.com>
2020-11-23 16:51:50 -0500

    create event filter component (in progress)

---
+++ front-end/src/components/EventFilter/EventFilter.jsx
@@ -0,0 +1,107 @@
+import { useState } from 'react';
+
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Form } from 'react-bootstrap';
+
+const events = [
+  {id: 1, gender: 'Male', level:"Intermediate", date: '2020-11-25'},
+  {id: 2, gender: 'Female', level:"Beginner", date: '2020-11-25'},
+  {id: 3, gender: 'Other', level:"Open", date: '2020-12-03'},
+  {id: 4, gender: 'Female', level:"Open", date: '2020-11-25'},
+  {id: 5, gender: 'Co-ed', level:"Open", date: '2020-11-25'},
+  {id: 6, gender: 'Male', level:"Beginner", date: '2020-11-25'},
+  {id: 7, gender: 'Co-ed', level:"Intermediate", date: '2020-12-03'},
+  {id: 8, gender: 'Female', level:"Advanced", date: '2020-11-25'},
+  {id: 9, gender: 'Other', level:"Intermediate", date: '2020-11-25'},
+  {id: 10, gender: 'Male', level:"Beginner", date: '2020-12-03'},
+  {id: 11, gender: 'Co-ed', level:"Advanced", date: '2020-11-25'},
+  {id: 12, gender: 'Female', level:"Beginner", date: '2020-12-03'},
+  {id: 13, gender: 'All', level:"Intermediate", date: '2020-11-25'},
+  {id: 14, gender: 'Male', level:"Beginner", date: '2020-12-03'},
+  {id: 15, gender: 'Male', level:"Intermediate", date: '2020-11-25'},
+];
+
+// Props to change filter state
+export default function EventFilter (props) {
+  
+  // Temporary put the state here, eventually the state will be 
+  const [filter, setFilter] = useState({}); 
+
+  let filteredData = filter.gender ? data.filter(item => item.gender === filter.gender) : data;
+  filteredData = filter.level ? filteredData.filter(item => item.level === filter.level) : filteredData;
+
+
+  // The functions below  should be in be EventIndex Component
+
+  const filterEvents = () => {
+    let filteredEvents = events;
+    for (let category in filter) {
+      filteredEvents = filter[category] ? events.filter(event => event[category] === filterObj[category]) : filteredEvents;
+    }
+    return filteredEvents;
+  }
+
+  const makeEventsByDateObj = (events) => {
+    let dates = [...new Set(events.map(event => event.date).sort())];
+    const eventsByDate = {};
+    for (let date of dates) {
+        eventsByDate[date] = events.filter(item => item.date === date)
+    }
+    return eventsByDate;
+  }
+
+  const filteredEvents = filterEvents();
+  const eventsByDate = makeEventsByDateObj(filteredEvents);
+
+    
+  const eventElements = Object.keys(eventsByDate).map((date) => {
+    return (
+      <div key={date}>
+      <h3>{date}</h3>
+      {
+        eventsByDate[date].map(event => {
+          return (
+            <div key={event.id}>
+            <p>{event.gender} | {event.level}</p>
+            <p></p>
+            </div>
+          )
+        })
+      }
+      </div>
+    )
+  });
+
+
+  return (
+
+    <div className='App'>
+    <div className='App-header'>
+    
+    <Form>
+      <div onChange={(e) => setFilter(prev => ({...prev, gender: e.target.value}))} className="mb-3">
+        <Form.Label>Gender</Form.Label>
+        <Form.Check type="radio" value="" name="gender" label="(Show All)" defaultChecked />
+        <Form.Check type="radio" value="Male" name="gender" label="Male" /> 
+        <Form.Check type="radio" value="Female" name="gender" label="Female" /> 
+        <Form.Check type="radio" value="Other" name="gender" label="Other" /> 
+        <Form.Check type="radio" value="Co-ed" name="gender" label="Co-ed" /> 
+      </div>
+      <div onChange={(e) => setFilter(prev => ({...prev, level: e.target.value}))} className="mb-3">
+        <Form.Label>Level</Form.Label>
+        <Form.Check type="radio" value="" name="level" label="(Show All)" defaultChecked />
+        <Form.Check type="radio" value="Beginner" name="level" label="Beginner" />
+        <Form.Check type="radio" value="Intermediate" name="level" label="Intermediate" />
+        <Form.Check type="radio" value="Advanced" name="level" label="Advanced" />
+        <Form.Check type="radio" value="Open" name="level" label="Open" />
+      </div>
+    </Form>
+      <div>
+        {eventElements.length ? eventElements : <p>Currently no event available with this criteria.</p> }
+      </div>
+      </div>
+    </div>);
+};
+
+export default App;
