ef0250bdabe74c748e9cfb3550743e0bf7f0fc70
Bill Watters <billywatters13@gmail.com>
2020-11-22 19:01:01 -0500

    Add comments and delete consolelog

--- back-end/src/routes/owners.js
+++ back-end/src/routes/owners.js
@@ -8,18 +8,20 @@ module.exports = db => {
       gender_restriction, referee, additional_info} = req.body  
 
       let location 
-       
+      //Formatting variables for api request
       const geocodeAddress = address.replace(/\s/g, '+') 
       const geocodeCity = city.replace(/\s/g, '+') 
+
       //Retriving lat and long from google api
       axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${geocodeAddress},+${geocodeCity},+CA&key=AIzaSyCiH--m_9xGKiwCJxv9nIgg1PYanFgTNL8`)
       .then((res) => {
         const lat = res.data.results[0].geometry.location.lat
         const long = res.data.results[0].geometry.location.lng
+        //Creating (x,y) format needed for psql point data type in psql
         location = `(${lat}, ${long})` 
       })
-      .then(() => console.log(location)) 
       .then(() => {
+        //Making a new event
         db.query(` 
         INSERT INTO events (owner_id , date, start_time, end_time, title,
           address, city, province, current_participants, max_participants, skill_level, 
