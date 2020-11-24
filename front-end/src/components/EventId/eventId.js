51d08d3440f866fd0e51f56bd9542d56e82820ac
Bill Watters <billywatters13@gmail.com>
2020-11-24 20:04:30 +0000

    Added google map with a static location. currently takes up most of the page

--- front-end/src/components/EventId/eventId.js
+++ front-end/src/components/EventId/eventId.js
@@ -1,6 +1,7 @@
 import { React, useState, useEffect } from 'react'; 
 import axios from 'axios';
 import {useParams} from 'react-router-dom';
+import MapContainer from '../MapContainer/MapContainer'
 import './eventId.scss';
 
 export default function EventId (props) { 
@@ -16,26 +17,22 @@ export default function EventId (props) {
     const event = `http://localhost:8001/api/events/${eventId}`
     const team = `http://localhost:8001/api/events/${eventId}/teams`
     const comment = `http://localhost:8001/api/events/${eventId}/comments`
-    console.log(eventId)
     //Request to plug in to axios.all
     const eventRequest = axios.get(event)
     const teamRequest = axios.get(team, {eventId}) 
     const commentRequest = axios.get(comment, {eventId})
-    console.log('before axios')
     //Making all 3 requests
     axios.all([eventRequest, teamRequest, commentRequest])
     .then(axios.spread((...responses) => {
-      console.log('Check')
        //Request data
        const eventData = responses[0] 
        const teamData = responses[1] 
        const commentData = responses[2]  
-       console.log(responses)
       
       //Destructuring data from request
       const {id, owner_id, date, start_time, end_time, additional_info, address, city, current_participants, gender_restriction, location, max_participants, 
       province, referee, skill_level, title} = eventData.data[0] 
-      
+      console.log('location', location)
       //Adding data to the setstate
       setState({...state, id, owner_id, date, start_time, end_time, additional_info, address, city, current_participants, gender_restriction, location, max_participants, 
         province, referee, skill_level, title})  
@@ -84,12 +81,10 @@ export default function EventId (props) {
       setTeam2({...team2, goalies: goalies2, strikers: strikers2, defenders: defenders2, midfielders: midfielders2 }) 
       
       //Formatting Comments
-      console.log(commentData)
       const commentFormatted = commentData.data.map((comment, index) => ({
           ...comment, 
           fullName: `${comment.first_name} ${comment.last_name}`
       })) 
-      console.log(commentFormatted)
       setComments(commentFormatted) 
      })) 
   }, [])
@@ -100,7 +95,8 @@ export default function EventId (props) {
       <h1> {state.title} </h1> 
       <h1> {team1.goalies}</h1>
       <h1> {comments[0].fullName} </h1> 
-      <h1> {comments[0].comment} </h1>
+      <h1> {comments[0].comment} </h1> 
+      <MapContainer location={state.location}/>
     </section>
   )
 }
