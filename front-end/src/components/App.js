4fb0feba2f5e4616a1e32ad6ff7c9420a66ad23b
ozgeguner <ozgecaylioglu@gmail.com>
2020-11-27 21:40:56 +0000

    rearranged routers

--- front-end/src/components/App.js
+++ front-end/src/components/App.js
@@ -3,34 +3,32 @@ import axios from 'axios';
 import {
   BrowserRouter as Router,
   Switch,
-  Route
+  Route,
+  Redirect,
   } from "react-router-dom";
 import Login from './Login/LoginForm'
 import EventId from './EventId/eventId'
 import Register from './Register/RegisterForm'
 import ProfileForm from './Profile/ProfileForm'
 import EventsIndex from './Events/EventsIndex';
-import CreateEvent from './Events/CreateEvent';
-import Navigation from './Navigation/Navigation';
-import EditEvent from './Events/EditEvent';
 import MyEventsIndex from './MyEvents/MyEventsIndex';
 import Main from './Main/Main';
-
+import EditEvent from './Events/EditEvent'
+import CreateEvent from './Events/CreateEvent'
 import { useState , useEffect} from 'react';
 
 export default function App(props) {
+
+
   const [islogin, setisLogin] = useState(false)
   const [currentUser, setCurrentUser] = useState(JSON.parse(window.localStorage.getItem('userData')));
 
+  console.log('before useEffect', currentUser)
 
   useEffect(() => {
       axios.get('http://localhost:8001/api/cookies', {withCredentials:true}).then((res) => 
       { 
-        // console.log('before',islogin)
-      // console.log('aftertrue',islogin)
-        return setCurrentUser(prev => ({...prev ,user : res.data}))
-        //return;
-        
+        return setCurrentUser(prev => ({...prev ,user : res.data}))  
       })
     },[islogin])
 
@@ -38,61 +36,41 @@ export default function App(props) {
     <div className="App">
       <Router>
         <Switch>
-
         <Route exact path='/'>
             <Main />
-          </Route>
-          <Route exact path='/owners/events/edit/:eventId' render= {(props) => <EditEvent 
-          eventId={props.match.params.eventId}
-          currentUser = {currentUser}
-          />} >
-
           </Route>
           <Route path='/login'>
             <Login 
             islogin ={islogin}
             setisLogin = {setisLogin} />
           </Route>
-
           <Route path='/register'>
             <Register />
           </Route> 
           <Route exact path='/profile'>
             <ProfileForm  currentUser = {currentUser}
             /> 
-          </Route> 
-
-          <Route path='/navigation'>
-            <Navigation
-            /> 
           </Route>
-
           <Route exact path='/events' > 
-          <EventsIndex  
-           currentUser = {currentUser}/> 
-          </Route>
-          
-          <Route exact path='/owners/events/new' >
-            <CreateEvent currentUser = {currentUser}/>
-          </Route>
-          <Route exact path='/events/:eventId' > 
-            <EventId currentUser = {currentUser}/>
-            </Route>
-            <Route>
            <EventsIndex  
             currentUser = {currentUser}/> 
           </Route>
+          <Route exact path='/owners/events/new' >
+            <CreateEvent currentUser = {currentUser}/>
+          </Route>
+          
           <Route exact path='/my-events/:screen' > 
           < MyEventsIndex currentUser = {currentUser}/>
         </Route > 
           <Route exact path='/events/:eventId' render={(props) => <EventId eventId={props.match.params.eventId}/>} /> 
+          
+          <Route exact path='/owners/events/edit/:eventId' render= {(props) => <EditEvent 
+          eventId={props.match.params.eventId}
+          currentUser = {currentUser}
+          />} />
         </Switch>
       </Router>
     </div>
     
   );
 }
-
-//id = {currentUser.user.id}
-// first_name = {currentUser.user.first_name}
-// last_name = {currentUser.user.last_name}
