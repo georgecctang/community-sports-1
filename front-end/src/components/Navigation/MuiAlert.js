0ad9f8ef2598ab848b036b32b15cc24b6b08f692
Bill Watters <billywatters13@gmail.com>
2020-11-28 02:15:50 +0000

    Add functional confirmation message

--- front-end/src/components/Navigation/MuiAlert.js
+++ front-end/src/components/Navigation/MuiAlert.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Alert from '@material-ui/lab/Alert';
 import Button from '@material-ui/core/Button';
+import { propTypes } from 'react-bootstrap/esm/Image';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -12,10 +13,10 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export default function ActionAlerts() {
+export default function ActionAlerts(props) {
   const classes = useStyles();
 
   return (
-    <Alert onClose={() => {}}>This is a success alert — check it out!</Alert>
+    <Alert onClose={() => {props.hide(false)}}>This is a success alert — check it out!</Alert>
   );
 }
--- front-end/src/components/Navigation/Navigation.js
+++ front-end/src/components/Navigation/Navigation.js
@@ -1,4 +1,4 @@
-import { Modal, Button} from 'react-bootstrap'
+import { Modal, Button } from 'react-bootstrap'
 import { useState, useEffect } from 'react';
 import './Navigation.scss'
 import field from './field.jpeg'
@@ -6,39 +6,33 @@ import axios from 'axios';
 import { Redirect } from 'react-router-dom';
 import ActionAlerts from './MuiAlert'
 
-export default function Navigation (props) {
+export default function Navigation(props) {
   const [show, setShow] = useState(false)
   const [confirm, setConfirm] = useState(false)
-  const eventId = props.eventId.id 
+  const eventId = props.eventId.id
   //Blue is Team 1 Red is Team 2
   let teamId
   if (props.team === 'Blue') {
-    teamId = 1   
+    teamId = 1
   } else {
     teamId = 2
   }
-  
-  function counter (position) { //--> maybe we need current user name ?
+  function counter(position) { //--> maybe we need current user name ?
     //Get player ID to assign to position
-    axios.get('http://localhost:8001/api/cookies', {withCredentials:true})
+    axios.get('http://localhost:8001/api/cookies', { withCredentials: true })
       .then(res => {
         const player_id = res.data.id
-        console.log('eventid', eventId, 'playerid', player_id, 'positon selected', position,'teamID', teamId)
-        return axios.post(`http://localhost:8001/api/users/events/${eventId}/create`, {teamNumber: teamId, position: position, userId: player_id})
-        .then((res) => {
-          console.log(res)
-          if (res === 'post okay'){
-            setConfirm(true)
-          }
-        })
+        console.log('eventid', eventId, 'playerid', player_id, 'positon selected', position, 'teamID', teamId)
+        axios.post(`http://localhost:8001/api/users/events/${eventId}/create`, { teamNumber: teamId, position: position, userId: player_id })
       })
-  } 
+  }
+
 
 
   return (
     <>
       <Button variant="primary" onClick={() => setShow(true)}>
-      Join Team {props.team}
+        Join Team {props.team}
       </Button>
       <Modal
         show={show}
@@ -49,35 +43,67 @@ export default function Navigation (props) {
           <Modal.Title id="title">Choose your position on the {props.team.toLowerCase()} team!</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <div id='alert' className={`alert ${confirm ? 'alert-show' : 'alert-hide'}`} >
-          <ActionAlerts />
+          <div id='alert' className={`${confirm ? 'alert-show' : 'alert-hide'}`} >
+            <ActionAlerts  hide={setConfirm}/>
           </div>
-          <Button id="position-goalie" size="sm" onClick={(event) => { event.preventDefault();
-                                                                        counter('Goalie');
-                                                                        }}> goalie </Button>
-          <Button id="position-defender_1" size="sm" onClick={(event) => { event.preventDefault();
-                                                                        counter('Defender')}}> defender 1 </Button>
-          <Button id="position-defender_2" size="sm" onClick={(event) => { event.preventDefault();
-                                                                        counter('Defender')}}> defender 2 </Button>
-          <Button id="position-defender_3" size="sm" onClick={(event) => { event.preventDefault();
-                                                                        counter('Defender')}}> defender 3 </Button>
-          <Button id="position-defender_4" size="sm" onClick={(event) => { event.preventDefault();
-                                                                        counter('Defender')}}> defender 4 </Button>
-          <Button id="position-midfield_1" size="sm" onClick={(event) => { event.preventDefault();
-                                                                        counter('Midfielder')}}> midfield 1 </Button>
-          <Button id="position-midfield_2" size="sm" onClick={(event) => { event.preventDefault();
-                                                                        counter('Midfielder')}}> midfield 2 </Button>
-          <Button id="position-midfield_3" size="sm" onClick={(event) => { event.preventDefault();
-                                                                        counter('Midfielder')}}> midfield 3 </Button>
-          <Button id="position-midfield_4" size="sm" onClick={(event) => { event.preventDefault();
-                                                                        counter('Midfielder')}}> midfield 4 </Button>
-          <Button id="position-striker_1" size="sm" onClick={(event) => { event.preventDefault();
-                                                                        counter('Striker')}}> striker 1 </Button>
-          <Button id="position-striker_2" size="sm" onClick={(event) => { event.preventDefault();
-                                                                        counter('Striker')}}> striker 2 </Button>
+          <Button id="position-goalie" size="sm" onClick={(event) => {
+            event.preventDefault();
+            counter('Goalie');
+            setConfirm(true)
+          }}> goalie </Button>
+          <Button id="position-defender_1" size="sm" onClick={(event) => {
+            event.preventDefault();
+            counter('Defender')
+            setConfirm(true)
+          }}> defender 1 </Button>
+          <Button id="position-defender_2" size="sm" onClick={(event) => {
+            event.preventDefault();
+            counter('Defender')
+            setConfirm(true)
+          }}> defender 2 </Button>
+          <Button id="position-defender_3" size="sm" onClick={(event) => {
+            event.preventDefault();
+            counter('Defender')
+            setConfirm(true)
+          }}> defender 3 </Button>
+          <Button id="position-defender_4" size="sm" onClick={(event) => {
+            event.preventDefault();
+            counter('Defender')
+            setConfirm(true)
+          }}> defender 4 </Button>
+          <Button id="position-midfield_1" size="sm" onClick={(event) => {
+            event.preventDefault();
+            counter('Midfielder')
+            setConfirm(true)
+          }}> midfield 1 </Button>
+          <Button id="position-midfield_2" size="sm" onClick={(event) => {
+            event.preventDefault();
+            counter('Midfielder')
+            setConfirm(true)
+          }}> midfield 2 </Button>
+          <Button id="position-midfield_3" size="sm" onClick={(event) => {
+            event.preventDefault();
+            counter('Midfielder')
+            setConfirm(true)
+          }}> midfield 3 </Button>
+          <Button id="position-midfield_4" size="sm" onClick={(event) => {
+            event.preventDefault();
+            counter('Midfielder')
+            setConfirm(true)
+          }}> midfield 4 </Button>
+          <Button id="position-striker_1" size="sm" onClick={(event) => {
+            event.preventDefault();
+            counter('Striker')
+            setConfirm(true)
+          }}> striker 1 </Button>
+          <Button id="position-striker_2" size="sm" onClick={(event) => {
+            event.preventDefault();
+            counter('Striker')
+            setConfirm(true)
+          }}> striker 2 </Button>
           <img src={field} alt="field" className="img-fluid" />
         </Modal.Body>
-    </Modal>
+      </Modal>
     </>
   )
 }
