cbb0cc377c5a068d2828ed250b6de97b8886a646
Bill Watters <billywatters13@gmail.com>
2020-12-01 22:11:13 +0000

    Change comment order in seed file

--- back-end/src/db/seeds/05_comments.sql
+++ back-end/src/db/seeds/05_comments.sql
@@ -1,13 +1,13 @@
 INSERT INTO comments ( user_id, event_id, comment, time) 
 VALUES (7, 1, 'Wow this looks cool! Id go for sure if I was avalible.', '2020-10-22 10:50:54'),
-(4, 8, 'I LOVE SOCCER GIRL! I broke my foot last week though.', '2020-11-01 10:23:54'), 
 (2, 3,'Seems interesting!', '2020-11-20 11:22:33'),
-(12, 4, 'Hmm, why not.','2020-11-19 10:11:12'), 
 (18, 3, 'That was a great experience!', '2020-12-20 17:10:00'), 
+(12, 4, 'Hmm, why not.','2020-11-19 10:11:12'), 
+(11, 4, 'Wait? Is this Neville Gallimore from the Cowboys!?','2020-11-20 18:30:00'), 
+(20, 4, 'Im soooo IN','2020-11-20 18:30:00'), 
 (7, 7, 'Whaaaat this is nuts','2020-11-20 18:30:00'), 
 (12, 7, 'WOW, Im not good enough though.','2020-11-20 18:30:00'), 
 (19, 7, 'Id go but it would probably be a little too easy for me sorry...','2020-11-20 18:30:00'), 
-(14, 9, 'I suck at soccer but Im good at drinking. IM IN','2020-11-20 18:30:00'), 
 (9, 8, 'I cant wait!','2020-11-20 18:30:00'), 
-(11, 4, 'Wait? Is this Neville Gallimore from the Cowboys!?','2020-11-20 18:30:00'), 
-(20, 4, 'Im soooo IN','2020-11-20 18:30:00'); 
+(4, 8, 'I LOVE SOCCER GIRL! I broke my foot last week though.', '2020-11-01 10:23:54'), 
+(14, 9, 'I suck at soccer but Im good at drinking. IM IN','2020-11-20 18:30:00'); 
