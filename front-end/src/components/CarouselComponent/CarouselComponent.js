de46b75707970f58bef2d13472be0b6a8c6275ee
ozgeguner <ozgecaylioglu@gmail.com>
2020-12-01 00:30:00 +0000

    added caurosel for homepage

---
+++ front-end/src/components/CarouselComponent/CarouselComponent.js
@@ -0,0 +1,31 @@
+import { Carousel } from 'react-bootstrap';
+import './CarouselComponent.scss';
+
+export default function CarouselComponent (props) {
+
+return (
+  <>
+  <div class="overlay"><span>Join the locals for the games<br></br> you've always loved.</span></div>
+
+  <Carousel controls={false} pause={false} interval={8000}> 
+
+    <Carousel.Item  >
+    <div className="carousel-image1">
+
+    </div>
+    </Carousel.Item>
+    <Carousel.Item  >
+    <div className="carousel-image2">
+
+    </div>
+    </Carousel.Item>
+    <Carousel.Item  >
+    <div className="carousel-image3">
+ 
+    </div>
+    </Carousel.Item>  
+
+  </Carousel>
+  </>
+  )
+} 
